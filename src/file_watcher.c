7259b71c807706f95af9f34956aa8183aa945c24
cabelitos <cabelitos@uol.com.br>
2014-01-11 11:55:54 -0200

    Scheduling file changes. Do not commit the changes immediately, probably the file will be changed again. Doing this way we avoid doing a lot of IO operations and the commits will decrease drastically.

--- src/file_watcher.c
+++ src/file_watcher.c
@@ -7,9 +7,36 @@
 
 static GHashTable *_file_monitors = NULL;
 static int _refcount = 0;
+static guint _timeout_id = 0;
 static File_Keeper *_keeper = NULL;
+static GSList *_changed_files = NULL;
 
 #define FILE_WATCHER_FOLDER "file_keeper"
+#define EXPIRE_TIME 3000
+
+typedef struct _File_Changed_Info {
+	char *path;
+	guint f_hash; /* for faster comparison at _file_watcher_file_changed_info_already_marked()*/
+	gboolean deleted;
+} File_Changed_Info;
+
+static File_Changed_Info *_file_watcher_file_changed_info_new(char *path,
+	guint f_hash, gboolean deleted)
+{
+	File_Changed_Info *info = g_malloc(sizeof(File_Changed_Info));
+	info->path = path;
+	info->deleted = deleted;
+	info->f_hash = f_hash;
+	return info;
+}
+
+static void _file_watcher_free_changed_info_and_commit(gpointer data)
+{
+	File_Changed_Info *info = data;
+	file_keeper_save_changes(_keeper, info->path, info->deleted);
+	g_free(info->path);
+	g_free(info);
+}
 
 static void _file_watcher_key_destroy(gpointer data)
 {
@@ -50,10 +77,39 @@ void file_watcher_shutdown(void)
 		_refcount = 0;
 		return;
 	}
+	if (_timeout_id)
+		g_source_remove(_timeout_id);
 	g_hash_table_destroy(_file_monitors);
+	g_slist_free_full(_changed_files,
+		_file_watcher_free_changed_info_and_commit);
 	file_keeper_free(_keeper);
 	_file_monitors = NULL;
 	_keeper = NULL;
+	_changed_files = NULL;
+	_timeout_id = 0;
+}
+
+static gboolean _file_watcher_save_timeout(gpointer data)
+{
+	_timeout_id = 0;
+	g_slist_free_full(_changed_files,
+		_file_watcher_free_changed_info_and_commit);
+	_changed_files = NULL;
+	(void)data;
+	return FALSE;
+}
+
+static gboolean _file_watcher_file_changed_info_already_marked(guint f_hash)
+{
+	GSList *itr = _changed_files;
+	File_Changed_Info *info;
+
+	for (itr = _changed_files; itr; itr = itr->next) {
+		info = itr->data;
+		if (info->f_hash == f_hash)
+			return TRUE;
+	}
+	return FALSE;
 }
 
 static void _file_watcher_monitor_changed(GFileMonitor *monitor, GFile *file,
@@ -62,6 +118,7 @@ static void _file_watcher_monitor_changed(GFileMonitor *monitor, GFile *file,
 	char *path;
 	GFileType type;
 	gboolean deleting = FALSE;
+	guint f_hash;
 
 	if (event == G_FILE_MONITOR_EVENT_CHANGES_DONE_HINT ||
 		event == G_FILE_MONITOR_EVENT_UNMOUNTED ||
@@ -82,10 +139,22 @@ static void _file_watcher_monitor_changed(GFileMonitor *monitor, GFile *file,
 		deleting = TRUE;
 	} else if (event == G_FILE_MONITOR_EVENT_CREATED)
 			file_watcher_add_watches(path);
+
 	/* We cannot track directories, only the files in it. */
-	if (type != G_FILE_TYPE_DIRECTORY)
-		file_keeper_save_changes(_keeper, path, deleting);
-	g_free(path);
+	if (type == G_FILE_TYPE_DIRECTORY) {
+		g_free(path);
+		return;
+	}
+	if (_timeout_id)
+		g_source_remove(_timeout_id);
+
+	f_hash = g_file_hash(file);
+	if (!_file_watcher_file_changed_info_already_marked(f_hash)) {
+		_changed_files = g_slist_prepend(_changed_files,
+			_file_watcher_file_changed_info_new(path, f_hash, deleting));
+	}
+
+	_timeout_id = g_timeout_add(EXPIRE_TIME, _file_watcher_save_timeout, NULL);
 	(void) other;
 	(void) monitor;
 	(void) data;
